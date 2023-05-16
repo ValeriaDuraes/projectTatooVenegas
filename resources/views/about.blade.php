747f5639ba4663667558929eee0bd36eba3963dd
Ana Beatriz <anabeatrizduraes326@gmail.com>
2023-05-15 22:20:31 -0300

    add_sobre_mim

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,1 +1,108 @@
-<h1>Página sobre mim</h1>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Sobre mim</title>
+    <link rel="stylesheet" href="flex.css" />
+    <link rel="stylesheet" href="css.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+    <div id="navbar" class="flex-row box content-space-between content-padding-20">
+        <div id="logo" class="hspace-20"></div>
+        <div class="">
+            <ul class="flex-row vspace-15">
+                <li><a class="hspace-20" href="#">Portifólio</a></li>
+                <li><a class="hspace-20" href="index.html">Projetos</a></li>
+                <li><a class="hspace-20" href="#">Feedback</a></li>
+                <li><a class="hspace-20" href="#">Contato</a></li>
+            </ul>
+        </div>
+    </div>
+    <header id="cabecalho" class="flex-row box branco content-space-between content-hcenter">
+        <div id="text-header" class="branco content-hcenter center">
+            <p>Tatuador de Marília-SP</p>
+            <p>Adquirido + 11 Estilos</p>
+            <p>Adquirido + 52 Prêmios</p>
+        </div>
+        <div id="img-capa" class="flex-row"></div>
+    </header>
+
+    <section class="branco flex-row content-hcenter">
+        <div class="branco center">
+            <p class="branco space-10">
+                was popularised in the 1960s with the release of Letraset sheets
+                containing Lorem electronic.
+            </p>
+            <p class="branco space-20">
+                br Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy text ever
+                since the 1500s.
+            </p>
+            <p class="branco space-10">
+                br Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy text ever
+                since the 1500s.
+            </p>
+        </div>
+    </section>
+    <section class="flex-row box branco content-space-between fundo-branco no-wrap">
+        <div id="branco-img" class="fundo-branco"></div>
+        <div class="fundo-branco center">
+            <p class="fundo-branco space-10">
+                was popularised in the 1960s with the release of Letraset sheets
+                containing Lorem electronic.
+            </p>
+            <p class="fundo-branco space-10">
+                br Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy text ever
+                since the 1500s.
+            </p>
+            <p class="fundo-branco space-10">
+                br Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy text ever
+                since the 1500s.
+            </p>
+        </div>
+    </section>
+
+    <section class="flex-row box branco content-space-between no-wrap">
+        <div class="center">
+            <p class="space-10">
+                was popularised in the 1960s with the release of Letraset sheets
+                containing Lorem electronic.
+            </p>
+            <p class="space-10">
+                br Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy text ever
+                since the 1500s.
+            </p>
+            <p class="space-10">
+                br Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy text ever
+                since the 1500s.
+            </p>
+        </div>
+        <div id="preto-img" class=""></div>
+    </section>
+    <footer>
+        <div class="flex-col content-center">
+            <div class="flex-row">
+                <h1>TATTOO VENEGAS</h1>
+            </div>
+            <div class="flex-row space-40">
+                <a href=""><i class="fa-brands fa-whatsapp fa-2xl"></i></a>
+                <a href=""><i class="fa-brands fa-instagram fa-2xl"></i></a>
+                <a href=""><i class="fa-brands fa-twitter fa-2xl"></i></a>
+            </div>
+        </div>
+    </footer>
+</body>
+
+</html>
